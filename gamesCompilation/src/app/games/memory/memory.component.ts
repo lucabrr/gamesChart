dea24af0a4e047d900f18e2390be64f6a56cae0c
lucabrr <lucabrancato95@gmail.com>
2023-08-13 13:54:32 +0200

    fix reset button tris e commentato modale per test

--- gamesCompilation/src/app/games/memory/memory.component.ts
+++ gamesCompilation/src/app/games/memory/memory.component.ts
@@ -39,7 +39,7 @@ export class MemoryComponent implements OnInit {
   ngOnInit(): void {}
 
   start(): void {
-    this.modal.open(this.myModal);
+    //this.modal.open(this.myModal); for test
     this.isStarted = true;
     for (let i = 1; i <= 20; i++) {
       let y = i <= 10 ? i : i - 10;
